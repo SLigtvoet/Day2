acf810270eb3341052f5eb7747fa11784ab0d009
Sjoerd Ligtvoet <sjoerd_ligtvoet@hotmail.com>
2018-07-02 17:21:11 +0200

    test

---
+++ main.js
@@ -0,0 +1,15 @@
+
+    
+    
+    let numberInput = prompt("Please enter your favorite programming language", "input here").toLocaleLowerCase();
+    let numberCheck = numberInput
+
+    if (numberCheck === "javascript" ) {
+        console.log("I love Javascript");      
+    } else if (numberCheck === "php" ) {
+        console.log("I kinda not love PHP");  
+    } else {
+        console.log("Sorry, I mostly like Javascript!");
+        }
+
+    
