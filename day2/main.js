64c1862d2eab5d13ef42270b1974dfc6deeaa8ff
Sjoerd Ligtvoet <sjoerd_ligtvoet@hotmail.com>
2018-07-03 12:07:44 +0200

    day 2

---
+++ day2/main.js
@@ -0,0 +1,59 @@
+/* DIY for loop
+
+for (var i = 5; i < 51; i += 5) {
+    console.log(i);
+}
+*/
+
+
+/* DIY while loop
+var i = 5
+
+while (i < 56) {
+  console.log(i)
+  i += 5
+}
+*/
+
+/* DIY fruits
+
+let fruits = ['apples', 'oranges', 'pear', 'apricot'];
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log('I love ' + fruits[i]);
+}
+
+*/
+
+/* exercise meal
+let recipe = ['0: cut and bake vegetables in a frying pan', '1: stir 6 eggs and 100 gram of ricotta', '2: add ricotta to the baked vegetables', '3: after 5 minutes, turn frittata, sprinkle parmaggiano over it', '4: bake frittata, under a cover, and in the oven for 8 minutes']
+
+recipe.forEach(function(recipe) {
+  console.log(recipe)
+});
+*/
+
+/* rembert exercise
+var person = {
+  firstName: 'Rembert',
+  lastName: 'Boon'
+}
+console.log('Hello, I am ' + person.firstName + ' ' + person.lastName);
+
+*/
+
+
+/* Student exercise
+var student = {
+  firstName: 'Sjoerd',
+  age: 25,
+  ageInMonths() {
+    return `${this.age * 12}`
+  }
+}
+console.log();
+
+console.log("I am " + student.firstName + ' and I am ' + student.age + ' years old, or ' + student.ageInMonths() + ' months old');
+
+*/
+
